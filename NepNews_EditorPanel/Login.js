a2761240b38413e09d2649f149d55a385b7a8d0a
Harshana Bhandari <np03cs4a230186@heraldcollege.edu.np>
2025-03-31 11:47:46 +0545

    Editor Login Page, Home Page, Editing Page is completed

---
+++ NepNews_EditorPanel/Login.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the login button
+    const loginBtn = document.getElementById("loginBtn");
+
+    // Add event listener to the button
+    loginBtn.addEventListener("click", function () {
+        // Redirect to another page (replace with your actual page)
+        window.location.href = "Editor_MainPage.html"; 
+    });
+});
